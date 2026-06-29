2c62ebec6f89b572dbb1e613b47e1fcfcf6766b8
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2026-06-29 15:38:21 +0200

    hit / hurt box setup

---
+++ game/interaction/hit_hurt_boxes/hit_box.gd
@@ -0,0 +1,5 @@
+@tool
+class_name HitBox extends InteractionBox
+
+@export var damage:= 10.0
+@export var knockback:= 2.0
---
+++ game/interaction/hit_hurt_boxes/interaction_box.gd
@@ -0,0 +1,7 @@
+@tool
+@abstract class_name InteractionBox extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
