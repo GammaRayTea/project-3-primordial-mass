4957a46a9ca5f44cc889677f5b612feee987094d
GammaRayTea <raymond.g.n.dinh@gmail.com>
2026-07-14 00:35:25 +0200

    camera collision wip, visual stuff

--- game/player/camera.gd
+++ game/player/camera.gd
@@ -1,5 +1,7 @@
 class_name PlayerCam extends Camera3D
 
+
+
 @export var bounding_box:Vector2
 @export_range(0,0.2,0.0001)
 var return_weight:float
@@ -15,17 +17,53 @@ var height:float
 var current_time_until_reset:int = 0
 var current_return_weight:float = 0
 
+@export var ray_cast:RayCast3D
+@export var visualizer:Node3D
+
+@export var wall_margin:float = 0.5
+var target_position:Vector3 = Vector3(0,height,0)
+
+func _ready():
+	target_position = Vector3(0,height,0)
 func _update_position(_player_rotation:Quaternion) -> void:
 	
-	var target_position:Vector3 = -_player_rotation*Vector3.FORWARD * Vector3(bounding_box.x,0,bounding_box.y)
+	target_position = -_player_rotation*Vector3.FORWARD * Vector3(bounding_box.x,0,bounding_box.y)
+	ray_cast.target_position = Vector3(target_position.x,0,target_position.z)*1.5
+	
+
+
 	target_position.y = height
-	position = position.lerp(target_position,move_weight)
+
+	
 	current_time_until_reset = reset_time
 	current_return_weight = 0.0
 
+func _physics_process(_delta):
+	visualizer.position = target_position
+	position = position.lerp(target_position,move_weight)
+
+	if ray_cast.is_colliding():
+		var collision_point:Vector3 = ray_cast.get_collision_point()-global_position
+		if collision_point:
+
+#
+			#target_position.y = height
+			collision_point.y = height
+
+			print(global_position.distance_to(collision_point))
+			if position.distance_to(collision_point)< wall_margin:
+				target_position = collision_point-position.normalized()*(wall_margin)
+				print(collision_point-position.normalized()*(wall_margin))
+				target_position.y = height
+			position.y = height
+
+	
+
+
 func _move_to_center() -> void:
-	if current_time_until_reset <= 0:
-		current_return_weight = lerpf(current_return_weight,return_weight,0.01)
-		position = position.lerp(Vector3(0,height,0),current_return_weight)
-	else:
-		current_time_until_reset-= 1
+	pass
+	#if current_time_until_reset <= 0:
+		#current_return_weight = lerpf(current_return_weight,return_weight,0.01)
+		#position = position.lerp(Vector3(0,height,0),current_return_weight)
+	#else:
+		#current_time_until_reset-= 1
