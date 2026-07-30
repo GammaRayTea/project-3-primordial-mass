09608ae2a7db1952a4f2b6d03aae26e47de5edad
hyumilis <102694635+hyumilis@users.noreply.github.com>
2026-07-30 14:15:14 +0200

    changed the cost formula again and adjusted costs and multipliers

--- game/ui/upgrade_menu/upgrade_module.gd
+++ game/ui/upgrade_menu/upgrade_module.gd
@@ -70,8 +70,11 @@ func update_slot_amount() -> void:
 
 func update_data() -> void:
 	#TODO
-	@warning_ignore("narrowing_conversion")
-	current_cost = base_cost * (1 + cost_mulitplier * current_upgrade_level)
+	if current_upgrade_level == 0:
+		current_cost = base_cost
+	else:
+		@warning_ignore("narrowing_conversion")
+		current_cost = base_cost * pow(2, current_upgrade_level) * cost_mulitplier 
 	name_label.text = ItemAssets.stat_names[upgrade_stat]
 	cost_label.text = '[img]'+ ItemAssets.currency_icons[upgrade_currency] + '[/img] ' + str(current_cost)
 	description_label.text = ItemAssets.stat_descriptions[upgrade_stat]
